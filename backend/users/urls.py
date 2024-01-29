a5648f4714bb49dfc04fda3a35f007dafd9c462d
joshuatrefzer <121818554+joshuatrefzer@users.noreply.github.com>
2024-01-29 10:58:23 +0100

    html-template email/ login with token

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import register, activate_account
+from .views import register, activate_account, login
 
 urlpatterns = [
     path('register/', register, name='register'),
+    path('login/', login, name='login'),
     path('activate/<str:confirmation_token>/', activate_account, name='activate_account'),
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -11,6 +11,9 @@ from rest_framework.response import Response
 from .models import CustomUser
 from .serializers import UserSerializer
 from rest_framework.authtoken.models import Token
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
 from django.core.mail import send_mail
 from django.conf import settings
 
@@ -19,6 +22,7 @@ from django.conf import settings
 @permission_classes([AllowAny])
 def register(request):
     serializer = UserSerializer(data=request.data)
+    
     if serializer.is_valid():
         user = serializer.save()
         user.set_password(request.data['password'])
@@ -26,14 +30,43 @@ def register(request):
         user.confirmation_token = generate_token()
         user.save()
 
-        # Send confirmation email
+        # Render HTML template
         subject = 'Activate Your Account'
-        message = f'Click on the following link to activate your account: {settings.BASE_URL}/users/activate/{user.confirmation_token}/'
-        send_mail(subject, message, settings.EMAIL_HOST_USER, [user.email])
+        context = {'user': user, 'activation_link': f'{settings.BASE_URL}/users/activate/{user.confirmation_token}/'}
+        html_message = render_to_string('confirmation_email.html', context)
+        plain_message = strip_tags(html_message)
+
+        # Send confirmation email
+        email = EmailMultiAlternatives(subject, plain_message, settings.EMAIL_HOST_USER, [user.email])
+        email.attach_alternative(html_message, "text/html")
+        email.send()
 
         return Response({'success': 'Registration successful. A confirmation email has been sent.'}, status=status.HTTP_201_CREATED)
 
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+# @api_view(['POST'])
+# @permission_classes([AllowAny])
+# def register(request):
+#     serializer = UserSerializer(data=request.data)
+#     if serializer.is_valid():
+#         user = serializer.save()
+#         user.set_password(request.data['password'])
+#         user.is_activated = False
+#         user.confirmation_token = generate_token()
+#         user.save()
+
+#         # Send confirmation email
+#         subject = 'Activate Your Account'
+#         message = f'Click on the following link to activate your account: {settings.BASE_URL}/users/activate/{user.confirmation_token}/'
+#         send_mail(subject, message, settings.EMAIL_HOST_USER, [user.email])
+
+#         return Response({'success': 'Registration successful. A confirmation email has been sent.'}, status=status.HTTP_201_CREATED)
+
+#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 def generate_token():
@@ -51,7 +84,23 @@ def activate_account(request, confirmation_token):
     user.confirmation_token = None
     user.save()
     
+    #Redirect to Login Page 
     return Response({'success': 'Account successfully activated.', 'token': token.key, 'user': user.username}, status=status.HTTP_200_OK)   
 
 
+
+@api_view(['POST'])
+def login(request):
+    user = get_object_or_404(CustomUser, username =request.data['username'])
+    if user.is_activated:
+        if not user.check_password(request.data['password']):
+            return Response({"detail": "Not found."} , status=status.HTTP_400_NOT_FOUND)
+    
+        token, created = Token.objects.get_or_create(user=user)
+        token_key = token.key
+        
+        serializer = UserSerializer(instance=user)
+        return Response({"token": token_key, "user": serializer.data})
+    else:
+        return Response({"Your Account is not registred, or not activated yet"})
 
