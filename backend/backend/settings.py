06a2237d379950a84012c50862d17d573c93198f
joshuatrefzer <121818554+joshuatrefzer@users.noreply.github.com>
2024-03-20 18:36:32 +0100

    ssl?

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -60,6 +60,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = True
 DEFAULT_FROM_EMAIL = "Videoflix@noreply"
 EMAIL_HOST_USER = os.environ.get("DEVMAIL")
 EMAIL_HOST_PASSWORD = os.environ.get("DEVPW")
