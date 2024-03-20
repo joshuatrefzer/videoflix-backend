75fef6c9aadb2905247a64dda24ac5fc26c8e920
joshuatrefzer <121818554+joshuatrefzer@users.noreply.github.com>
2024-03-20 18:15:41 +0100

    settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -56,13 +56,13 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 
-# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-# EMAIL_HOST = "smtp.gmail.com"
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# DEFAULT_FROM_EMAIL = "Videoflix@noreply"
-# EMAIL_HOST_USER = os.environ.get("DEVMAIL")
-# EMAIL_HOST_PASSWORD = os.environ.get("DEVPW")
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = "Videoflix@noreply"
+EMAIL_HOST_USER = os.environ.get("DEVMAIL")
+EMAIL_HOST_PASSWORD = os.environ.get("DEVPW")
 
 # Application definition
 
@@ -139,14 +139,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = "Videoflix"
-EMAIL_HOST_USER = os.environ.get("DEVMAIL")
-EMAIL_HOST_PASSWORD = os.environ.get("DEVPW")
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_HOST = "smtp.gmail.com"
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# DEFAULT_FROM_EMAIL = "Videoflix"
+# EMAIL_HOST_USER = os.environ.get("DEVMAIL")
+# EMAIL_HOST_PASSWORD = os.environ.get("DEVPW")
 
 
 ROOT_URLCONF = "backend.urls"
