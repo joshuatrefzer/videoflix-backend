7016452106436973b0becfaea28533e213a6b7a4
joshuatrefzer <121818554+joshuatrefzer@users.noreply.github.com>
2024-03-20 08:51:26 +0100

    static

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from videos.views import VideoView, SearchView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,4 +32,4 @@ urlpatterns = [
     path('api/videos/search/' , SearchView.as_view(), name='search'),
     path('password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
     
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+] + staticfiles_urlpatterns() + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
