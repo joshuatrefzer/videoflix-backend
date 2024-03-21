c9c1ab1e348bce51c1f0cdc86e11b4f772e6fbeb
joshuatrefzer <121818554+joshuatrefzer@users.noreply.github.com>
2024-03-21 11:28:22 +0100

    bug fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -60,8 +60,7 @@ CORS_ALLOWED_HOSTS = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:4200",
     "https://localhost",
-     "https://videoflix.joshuatrefzer.de" ,
-    "videoflix.joshuatrefzer.de",
+    "https://videoflix.joshuatrefzer.de" ,
 ]
 
 
