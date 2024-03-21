8fa14b6b41116ef15d03ef30cfeaa76ec1366cd8
joshuatrefzer <121818554+joshuatrefzer@users.noreply.github.com>
2024-03-21 08:34:07 +0100

    mail

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -61,7 +61,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = "Videoflix@noreply"
+DEFAULT_FROM_EMAIL = os.environ.get("DEVMAIL")
 EMAIL_HOST_USER = os.environ.get("DEVMAIL")
 EMAIL_HOST_PASSWORD = os.environ.get("DEVPW")
 
@@ -143,7 +143,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = "Videoflix"
+DEFAULT_FROM_EMAIL = os.environ.get("DEVMAIL")
 EMAIL_HOST_USER = os.environ.get("DEVMAIL")
 EMAIL_HOST_PASSWORD = os.environ.get("DEVPW")
 
